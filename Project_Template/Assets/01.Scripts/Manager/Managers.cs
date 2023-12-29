0ea9f60ee3d74bb135181380a770c817b4e7853c
FixerSan <ggomarain1302@gmail.com>
2023-12-29 20:08:44 +0900

    Import: "프로젝트 임포트"
    아직 준비 중

---
+++ Project_Template/Assets/01.Scripts/Manager/Managers.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Managers : Singleton<Managers>
+{
+    private ResourceManager resource;
+    private PoolManager pool;
+    private UIManager ui;
+    private CoroutineManager routine;
+    private ScreenManager screen;
+
+    public static ResourceManager Resource { get { return Instance?.resource; } }
+    public static PoolManager Pool { get { return Instance?.pool; } }
+    public static UIManager UI { get { return Instance?.ui; } }
+    public static CoroutineManager Routine { get { return Instance?.routine; } }
+    public static ScreenManager Screen { get { return Instance?.screen; } }
+
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSplashScreen)]
+    public static void GameStart()
+    {
+        Managers.Resource.Load<>
+    }
+}
